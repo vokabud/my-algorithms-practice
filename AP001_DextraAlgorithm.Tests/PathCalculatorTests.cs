58a8fc948219e82ad4a48c309e51b42af9620f5e
vokabud <etecture.dubakov@extaccount.com>
2024-06-18 14:59:57 +0300

    Add extended test

--- AP001_DextraAlgorithm.Tests/PathCalculatorTests.cs
+++ AP001_DextraAlgorithm.Tests/PathCalculatorTests.cs
@@ -25,4 +25,35 @@ public class PathCalculatorTests
         // assert
         Assert.Equal(6, result);
     }
+
+    [Fact]
+    public void Sunshine_Extended()
+    {
+        // arrange
+        var notebook = new Node { Name = "notebook" };
+        var vinyl = new Node { Name = "vinyl" };
+        var poster = new Node { Name = "poster" };
+        var guitar = new Node { Name = "guitar" };
+        var drum = new Node { Name = "drum" };
+        var piano = new Node { Name = "piano" };
+
+        notebook.Edges.Add(new Edge { To = vinyl, Weight = 5 });
+        notebook.Edges.Add(new Edge { To = poster, Weight = 0 });
+
+        vinyl.Edges.Add(new Edge { To = guitar, Weight = 15 });
+        vinyl.Edges.Add(new Edge { To = drum, Weight = 20 });
+
+        poster.Edges.Add(new Edge { To = guitar, Weight = 30 });
+        poster.Edges.Add(new Edge { To = drum, Weight = 35 });
+
+        guitar.Edges.Add(new Edge { To = piano, Weight = 20 });
+
+        drum.Edges.Add(new Edge { To = piano, Weight = 10 });
+
+        // act
+        var result = new PathCalculator().CalculatePath(notebook, piano);
+
+        // assert
+        Assert.Equal(6, result);
+    }
 }
